a2d1f4e07355dd438968c99c442a6df68c35e53a
yusuke ota <33338085+yusuke-ota@users.noreply.github.com>
2020-08-25 09:06:10 +0900

    add trait
    実装忘れ防止のためtraitを設定。
    後で動かす。

--- code_writer/src/lib.rs
+++ code_writer/src/lib.rs
@@ -1,4 +1,15 @@
+use parser::command_type::CommandType;
+struct CodeWriter{
 
+}
+
+trait ICodeWriter{
+    fn new(stream: todo!()) -> Self;
+    fn set_file_name(&self, file_name: String);
+    fn write_arithmetic(&self, command: CommandType);
+    fn write_push_pop(&self, command: CommandType);
+    fn close(&self);
+}
 
 #[cfg(test)]
 mod tests {
--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,3 +1,29 @@
+mod parser;
+pub mod command_type;
+use command_type::CommandType;
+use std::fs::File;
+
+pub struct Parser{
+    stream: Vec<String>,
+    command: Option<String>,
+}
+
+trait IParser{
+    /// Create Parser from Files.
+    fn new(file: File) -> Self;
+    fn has_more_commands(&self) -> bool;
+    fn advance(&self);
+    fn command_type(&self) -> CommandType;
+    /// This function shouldn't call when Parser::command is CReturn.
+    fn arg1(&self) -> String;
+    /// This function should call when Parser::command is
+    /// * CPush
+    /// * CPop
+    /// * CFunction
+    /// * CCall
+    fn arg2(&self) -> i32;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
