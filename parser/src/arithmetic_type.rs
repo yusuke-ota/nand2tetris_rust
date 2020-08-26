e18a2d366aebb4c4e69d25c42db2b11c8999669f
yusuke-ota <33338085+yusuke-ota@users.noreply.github.com>
2020-08-26 19:52:37 +0900

    impl From<ArithmeticType> for &'static str.
    比較にシンボルがいりそう。
    インライン展開できないか考えてみる。

--- parser/src/arithmetic_type.rs
+++ parser/src/arithmetic_type.rs
@@ -34,16 +34,127 @@ impl TryFrom<&str> for ArithmeticType{
 impl From<ArithmeticType> for &'static str{
     fn from(arithmetic_type: ArithmeticType) -> Self {
         return match arithmetic_type {
-            // todo: 変換先のアセンブリを記載
-            ArithmeticType::Add => "D=D+A\n@0\nM=D",
-            ArithmeticType::Sub => "D=D-A\n@0\nM=D",
-            ArithmeticType::Neg => "",
-            ArithmeticType::Eq => "",
-            ArithmeticType::Gt => "",
-            ArithmeticType::Lt => "",
-            ArithmeticType::And => "",
-            ArithmeticType::Or => "",
-            ArithmeticType::Not => "",
+            ArithmeticType::Add => ADD,
+            ArithmeticType::Sub => SUB,
+            ArithmeticType::Neg => NEG,
+            ArithmeticType::Eq => eq(),
+            ArithmeticType::Gt => gt(),
+            ArithmeticType::Lt => lt(),
+            ArithmeticType::And => AND,
+            ArithmeticType::Or => OR,
+            ArithmeticType::Not => NOT,
         }
     }
+}
+
+const ADD: &'static str =
+    // スタックポインタの一番上のアドレス＋1が入っているアドレスを参照、Aに代入(Rust的に言うとA=&SP, M=SP)
+    // M=M-1(=一番上のアドレス)して、そのアドレスをAに代入
+    // DレジスタにM(=A(一番上のアドレス)の中身)を代入
+    // スタックポインタの一番上のアドレス＋1が入っているアドレスを参照、Aに代入
+    // DレジスタにM(=A(一番上のアドレス)の中身)を代入
+    // スタックの一番上に現在のDレジスタの値-Aレジスタの値を代入する
+    // スタックを一つ進める
+    "@SP\n\
+    AM=M-1\n\
+    D=M\n\
+    @SP\n\
+    AM=M-1\n\
+    M=D+M\n\
+    @SP\n\
+    M=M+1\n";
+const SUB: &'static str =
+    // A-Bがあったとき、M=D-MだとB-Aになってしまう
+    // 順番注意
+    "@SP\n\
+    AM=M-1\n\
+    D=A\n\
+    @SP\n\
+    AM=M-1\n\
+    M=M-D\n\
+    @SP\n\
+    M=M+1\n";
+const NEG: &'static str =
+    "@SP\n\
+    AM=M-1\n\
+    M=-M\n\
+    @SP\n\
+    M=M+1\n";
+const AND: &'static str =
+    "@SP\n\
+    AM=M-1\n\
+    D=A\n\
+    @SP\n\
+    AM=M-1\n\
+    M=D&M\n\
+    @SP\n\
+    M=M+1\n";
+const OR: &'static str =
+    "@SP\n\
+    AM=M-1\n\
+    D=A\n\
+    @SP\n\
+    AM=M-1\n\
+    M=D|M\n\
+    @SP\n\
+    M=M+1\n";
+const NOT: &'static str =
+    "@SP\n\
+    AM=M-1\n\
+    M=!M\n\
+    @SP\n\
+    M=M+1\n";
+// TODO:Labelを取得する
+fn eq() -> &'static str{
+    "@SP\n\
+    AM=M-1\n\
+    D=A\n\
+    @SP\n\
+    AM=M-1\n\
+    D=M-D\n\
+    @labelA\n\
+    D;JEQ\n\
+    M=0\n\
+    @labelB\n\
+    (labelA)\n\
+    M=-1\n\
+    (labelB)\n\
+    @SP\n\
+    M=M+1\n"
+}
+// TODO:Labelを取得する
+fn gt() -> &'static str{
+    "@SP\n\
+    AM=M-1\n\
+    D=A\n\
+    @SP\n\
+    AM=M-1\n\
+    D=M-D\n\
+    @labelA\n\
+    D;JGT\n\
+    M=0\n\
+    @labelB\n\
+    (labelA)\n\
+    M=-1\n\
+    (labelB)\n\
+    @SP\n\
+    M=M+1\n"
+}
+// TODO:Labelを取得する
+fn lt() -> &'static str{
+    "@SP\n\
+    AM=M-1\n\
+    D=A\n\
+    @SP\n\
+    AM=M-1\n\
+    D=M-D\n\
+    @labelA\n\
+    D;JLT\n\
+    M=0\n\
+    @labelB\n\
+    (labelA)\n\
+    M=-1\n\
+    (labelB)\n\
+    @SP\n\
+    M=M+1\n"
 }
