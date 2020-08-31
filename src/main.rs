ec6de6c05929c1187228305ed25ae87a73dd5ac2
yusuke ota <33338085+yusuke-ota@users.noreply.github.com>
2020-09-01 08:30:17 +0900

    impl main.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::env;
+use code_writer::{CodeWriter, ICodeWriter};
+use parser::command_type::CommandType;
+use parser::{Parser, IParser};
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() <= 1 {
+        panic!("Please input file path")
+    }
+    // &args[0]はこのバイナリの名前が入る
+    // &args[0] is this application name.
+    let mut code_writer = CodeWriter::new(&args[1]);
+    let mut parser = Parser::new(File::open(&args[1]).expect("Create file failed."));
+
+    // UNWRAP: path is "*/filename" or "filename", this .last() always success.
+    let file_name = args[1].split('/').last().unwrap().to_string();
+    code_writer.set_file_name(file_name);
+
+    while parser.has_more_commands(){
+        parser.advance();
+        process_command(&parser, &mut code_writer);
+    }
+
+    code_writer.close();
+}
+
+fn process_command(parser: &Parser, code_writer: &mut CodeWriter) {
+    match parser.command_type() {
+        CommandType::CArithmetic => {
+            let arithmetic_type = parser.arg1();
+            code_writer.write_arithmetic(arithmetic_type.as_str())
+        },
+        CommandType::CPush => {
+            let segment = parser.arg1();
+            let index = parser.arg2();
+            code_writer.write_push_pop(CommandType::CPush, segment, index)
+        }
+        CommandType::CPop => {
+            let segment = parser.arg1();
+            let index = parser.arg2();
+            code_writer.write_push_pop(CommandType::CPop, segment, index)
+        }
+        CommandType::CLabel => { unimplemented!() }
+        CommandType::CGoto => { unimplemented!() }
+        CommandType::CIf => { unimplemented!() }
+        CommandType::CFunction => { unimplemented!() }
+        CommandType::CReturn => { unimplemented!() }
+        CommandType::CCall => { unimplemented!() }
+    }
 }
